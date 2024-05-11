38642c131a151db1eec9e6f4d6ed0459ed36ea76
WizzyGeek <ojasscoding@gmail.com>
2024-05-12 02:14:41 +0530

    cleanup

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Hash Design Rig
 
 ## Challenge
-Given is discrete frequency distribution which is known to have little over 5 bits worth of information
-Extract the maximum possible information using as little physical bits as possible,
-mathematically maximise `entropy^2 / physical bits` where entropy is the informational entropy of the hash
-over the given dataset.
+Given is a discrete frequency distribution which is known to have little over 5 bits worth of information
+Extract the maximum possible information using as little physical bits as possible.
 
 ### Leaderboard
 
-Current best method is a modified hash algo from [huyane](https://github.com/HuyaneMatsu) with entropy 3.751290 and hash size of 4 bits
+Current best method is a modified hash algo from [huyane](https://github.com/HuyaneMatsu) with entropy `3.751290 bits` and hash size of `4 bits`.
+
+### Details
+
+Within data.json is given a relation between observed value and frequency of the observation, the keys are the observed value and the values are frequency
+
+Within main.py make your hashing function in-place of the function called `hash`. Implement a hashing strategy and run.
+
+Submit your hash functions via a PR.
--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import json
-import numpy as np
 import seaborn as sns
-import pandas as pd
 
 with open("data.json", "r") as fp:
     ctr = dict(map(lambda x: (int(x[0]), x[1]), json.load(fp).items()))
 
-# print(ctr)
-
 sns.set_theme()
 from math import exp, log, log1p, log2
-# sns.displot(ctr)
-# sns.barplot(x=["A", "B", "C"], y=[1, 3, 2])
 
 import matplotlib.pyplot as plt
-# plt.show()
 
-import scipy.stats as st
-
-counter_2 = {}
+### Current best
 
 def invert(n):
     out = 0
@@ -26,31 +17,42 @@ def invert(n):
         out |= ((n >> shift) & 0x1) << (3 - shift)
     return out
 
-n = 1
-for key, value in ctr.items():
-    # key = int((log((key + 1) / 5))) % 4
+def hash_huyane(key):
     if key != 0:
         low = key % 10
         high = key // 10
         key = (((low & 0xf) << 4) ^ (invert(high))) % 15 + 1
+    return key
+
+### Add your attempt here
+
+def hash(key):
+    if key == 0: return 0
+    return (key % 7) + 1
+
+counter_2 = {}
+for key, value in ctr.items():
+    key = hash(key)
+    # key = hash_huyane(key)
     counter_2[key] = counter_2.get(key, 0) + value
 
 ctr = counter_2
 
-# counter_2 = {}
-# for key, value in ctr.items():
-#     counter_2[key] = counter_2.get(key, 0) + value
-
-# ctr = counter_2
-
 s = sum(ctr.values())
 ent = -1 * sum((value / s) * (log2(value / s)) for value in ctr.values())
-print(ent, s, ctr)
+p = max(ctr).bit_length()
 
-# data = pd.DataFrame(ctr, columns=["x", "y"])
+print("entropy:", ent)
+print("validity:", s == 14167)
+print("max physical bits detected:", p)
+
+pv = (1 / (((1 << p) - (2 ** ent)) + 1)) * (1 << p) * (2 ** ent) / ((1 << (max(p, 5) - 5)) ** 2)
+print("perf value:", pv)
 
 sns.barplot(ctr)
 plt.show()
+
+# Misc
 
 # data = np.array(list(map(tuple, ctr.items())))
 # data = np.repeat(data[:,0], data[:,1])
@@ -61,5 +63,4 @@ plt.show()
 # ret = st.fit(st.expon, data, [(0, 100)])
 # print(ret)
 # ret.plot()
-
-plt.show()
+# plt.show()
